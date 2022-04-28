c5834b0270ba2295a29eded427b1932ae937fbae
Parricio <patriciogabriel colella@gmail.com>
2022-04-27 23:32:07 -0300

    09-homework-finished

--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-
 import './App.css';
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
+import { Route,Switch } from 'react-router-dom';
+import About from "../components/About.jsx"
+import Ciudad from "../components/Ciudad.jsx"
 
 const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
 
@@ -10,10 +12,11 @@ function App() {
   const [cities, setCities] = useState([]);
   function onClose(id) {
     setCities(oldCities => oldCities.filter(c => c.id !== id));
+
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
@@ -46,12 +49,29 @@ function App() {
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
+      <Route 
+        path="/" 
+        render={()=><Nav onSearch={onSearch}/>}>
+      </Route>
+
       <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
-        />
+        <Switch>
+        <Route
+          path="/about"
+          component={About}>
+        </Route>
+        <Route
+          exact
+          path="/"
+          render={()=><Cards cities={cities} onClose={onClose} />}>
+        </Route>
+      </Switch>
+      {
+      <Route
+        path='/ciudad/:ciudadId'
+        render={({match}) => <Ciudad city={onFilter(match.params.ciudadId)}  />} 
+      />
+      }
       </div>
       <hr />
     </div>
